6a14f808fbc7ceeef1213a47255e04458d4ca1a5
f3nrisw0lf <rodrickcorpuz@gmail.com>
2022-11-08 03:53:51 +0800

    Changed ProcFile

--- server.py
+++ server.py
@@ -9,7 +9,6 @@ from utility import import_tensorflow
 from dotenv import load_dotenv
 load_dotenv()
 
-
 app = Flask(__name__)
 limiter = Limiter(
     app,
@@ -96,4 +95,4 @@ def many_hate_prediction():
 
 
 if __name__ == "__main__":
-    serve(app, port=os.getenv("PORT", 5000))
+    serve(app)
