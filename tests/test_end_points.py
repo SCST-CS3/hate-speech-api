35dca1611bac6b9a6acb14b471b87ff76af13605
f3nrisw0lf <rodrickcorpuz@gmail.com>
2022-11-14 17:42:10 +0800

    Removed other json check, using only jsonschema

--- tests/test_end_points.py
+++ tests/test_end_points.py
@@ -18,7 +18,6 @@ def test_single_prediction(client):
     data = res.get_json()
 
     validate(data, single_prediction_schema)
-    assert data['is_hate_speech'] in [0, 1]
     assert res.status_code == 200
 
 
@@ -46,9 +45,5 @@ def test_many_prediction(client):
                       json=request)
     data = res.get_json()
 
-    first_index_value = dict(next(iter(data[0].values())))
-
     validate(data, many_prediction_schema)
-    assert first_index_value['is_hate_speech'] in [0, 1]
-    # assert first_index_value['original'] == request[0]
     assert res.status_code == 200
